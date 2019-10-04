f7c8f431dfb92f1a0afd0d416dd46b1ee51654eb
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2019-10-04 10:14:21 -0400

    Update setup_4nfv_ryu_controller.sh

--- setup_4nfv_ryu_controller.sh
+++ setup_4nfv_ryu_controller.sh
@@ -1,5 +1,5 @@
 cd ~
-wget https://raw.githubusercontent.com/YutingZ92/MasterProject/master/nfv_controller_4VNF.py
+wget https://raw.githubusercontent.com/YutingZ92/MasterProject/master/nfv_controller.py
 wget https://raw.githubusercontent.com/YutingZ92/MasterProject/master/4nfv.config
-mv nfv_controller_4VNF.py /tmp/ryu/ryu/app/
+mv nfv_controller.py /tmp/ryu/ryu/app/
 mv 4nfv.config /tmp/ryu/ryu/app/
