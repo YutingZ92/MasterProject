8e293166b37db7ba6cd03a5be8d8d8b687b72806
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-01-31 12:17:28 -0500

    Create setup_4nfv_ryu_controller.sh

---
+++ network2NVF4/PI/setup_4nfv_ryu_controller.sh
@@ -0,0 +1,5 @@
+cd ~
+wget https://raw.githubusercontent.com/YutingZ92/MasterProject/master/network2NVF4/PI/nfv_controller.py
+wget https://raw.githubusercontent.com/YutingZ92/MasterProject/master/network2NVF4/PI/4NVF.config
+mv nfv_controller.py /tmp/ryu/ryu/app/
+mv 4nfv.config /tmp/ryu/ryu/app/
