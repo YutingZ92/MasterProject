aaa28da37cb0ce62a54b2b50f960b538d6e0da96
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-03-13 14:04:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ var sockets = io.listen(server);
 sockets.on('connection', function (socket) {
 socket.emit('news', { hello: 'world' });//监听，一旦客户端连接上，即发送数据，第一个参数'new'为数据名，第二个参数既为数据  
 socket.on('my other event', function (data) {//得到client的 数据名为'my other event' 的数据
-console.log(data);
+console.log(data.my);
   });
 });
