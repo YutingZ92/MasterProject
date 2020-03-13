3cf4ac0ac8bb88c0e7eef6e9f7ea52366e35854c
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-03-13 13:59:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 var io = require('socket.io');
 var server = require('http').createServer();
 server.listen(80, "http://vm.public-node-1-128.ch-geni-net.lan.sdn.uky.edu");
-var socket = io.listen(server);
+var sockets = io.listen(server);
 
-socket.on('connection', function (socket) {
-socket.emit('news', { hello: 'world' });
-socket.on('my other event', function (data) {
+sockets.on('connection', function (socket) {
+socket.emit('news', { hello: 'world' });//监听，一旦客户端连接上，即发送数据，第一个参数'new'为数据名，第二个参数既为数据  
+socket.on('my other event', function (data) {//得到client的 数据名为'my other event' 的数据
 console.log(data);
   });
 });
