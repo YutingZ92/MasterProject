3be3af628be3b00b6f57299710094e929b204d21
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-03-13 13:22:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = require('http').createServer();
 server.listen(80, "http://vm.public-node-1-128.ch-geni-net.lan.sdn.uky.edu");
 var socket = io.listen(server);
 
-io.on('connection', function (socket) {
+socket.on('connection', function (socket) {
 socket.emit('news', { hello: 'world' });
 socket.on('my other event', function (data) {
 console.log(data);
