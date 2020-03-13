3c96494534af3aa96323004c66e88ef2c1d8624a
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-03-13 12:49:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
 
-server.listen(80);
+app.listen(80);
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/', function (req, res) {
