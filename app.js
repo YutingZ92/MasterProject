d380c4c2f087ad6b04c3e46b1dce31da7c54a4b2
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-03-13 14:25:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var io = require('socket.io');
 var server = require('http').createServer();
-server.listen(3000,"http://vm.public-node-1-128.ch-geni-net.lan.sdn.uky.edu");
+server.listen(80,"http://128.163.232.70");
 var sockets = io.listen(server);
 
 sockets.on('connection', function (socket) {
