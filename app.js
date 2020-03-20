2dca7e4c8275b37dbd2081d1e82c083bca254bd1
YutingZ92 <55571085+YutingZ92@users.noreply.github.com>
2020-03-19 22:55:28 -0400

    Update and rename server.js to app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var io = require('socket.io');
+var server = require('http').createServer();
+var sockets = io.connect('http://128.163.232.70:8000'); 
+//server.listen(8000,"http://128.163.232.70");
+//server.listen(80);
+
+
+sockets.on('connection', function (socket) {
+socket.emit('news', { hello: 'world' });//监听，一旦客户端连接上，即发送数据，第一个参数'new'为数据名，第二个参数既为数据  
+socket.on('my other event', function (data) {//得到client的 数据名为'my other event' 的数据
+console.log(data.my);
+  });
+});
